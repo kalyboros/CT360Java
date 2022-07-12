95a685c6c981937e5d3229341ceb3d8f9a85e47e
Dalibor Veselinović <daliborv@comtrade.com>
2022-07-12 15:17:02 +0200

    Initial commit

---
+++ src/main/java/com/assignments/ct360java/Ct360JavaApplication.java
@@ -0,0 +1,13 @@
+package com.assignments.ct360java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ct360JavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ct360JavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assignments/ct360java/Ct360JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignments.ct360java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ct360JavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
