66f574bee1a302bce69f8f331ea0361be948c4ba
daliborv <dadosa123@gmail.com>
2022-07-14 15:37:25 +0200

    Removed second and third task.

--- src/main/java/com/assignments/ct360java/task2and3/repos/CarRepository.java
+++
@@ -1,8 +0,0 @@
-package com.assignments.ct360java.task2and3.repos;
-
-import com.assignments.ct360java.task2and3.entities.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car, Integer> {
-
-}
