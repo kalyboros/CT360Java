287a21b1216b3c5a102dd01d3d17c24c65e91b65
daliborv <dadosa123@gmail.com>
2022-07-14 15:28:26 +0200

    Removed first task.

--- src/main/java/com/assignments/ct360java/Ct360JavaApplication.java
+++ src/main/java/com/assignments/ct360java/Ct360JavaApplication.java
@@ -1,20 +1,15 @@
 package com.assignments.ct360java;
 
-import com.assignments.ct360java.task1.exception_assignment.ExceptionAssignment;
-import com.assignments.ct360java.task1.exceptions.HorsepowerException;
-import com.assignments.ct360java.task1.exceptions.KilometersException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Ct360JavaApplication {
 
-    public static void main(String[] args) throws KilometersException, HorsepowerException {
+    public static void main(String[] args) {
 
         SpringApplication.run(Ct360JavaApplication.class, args);
-
-        ExceptionAssignment ea = new ExceptionAssignment();
-        ea.Assignment();
 
     }
 
